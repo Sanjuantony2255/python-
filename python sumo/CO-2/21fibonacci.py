76eaf46814c25c4f6edc52d2737441c476ebe116
Sanju Antony <146819534+Sanjuantony2255@users.noreply.github.com>
2024-02-02 11:37:17 +0530

    Add files via upload

---
+++ python sumo/CO-2/21fibonacci.py
@@ -0,0 +1,15 @@
+f=0
+s=1
+sum=0
+c=0
+n=int(input("Enter limit:"))
+if (n==1):
+    print("0")
+else:
+    print("0")
+    print("1")
+    for i in range(2,n):
+        sum=f+s
+        f=s
+        s=sum
+        print(sum)
